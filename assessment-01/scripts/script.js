cf85dc01f1c047d2dce7aa9a0e67aba1721f9672
jonahwhitney <jonahwhitney@yahoo.com>
2025-01-20 15:47:04 -0600

    Experimenting with JS and HTML assessment

---
+++ assessment-01/scripts/script.js
@@ -0,0 +1,7 @@
+function infoMercer() {
+  document.getElementById("intro").innerHTML="This big boy was born on December 15, 2023."
+}
+
+function infoQuinn() {
+  document.getElementById("intro").innerHTML="This beautiful girl was born on February 2, 2022."
+}
