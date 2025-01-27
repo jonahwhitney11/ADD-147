da3a5199e1840ac3c9eacff7365afbfed9f11624
jonahwhitney <jonahwhitney@yahoo.com>
2025-01-27 11:37:20 -0600

    Modified estimate function to take into account different number of rooms.

--- learn04-start/scripts/calculate.js
+++ learn04-start/scripts/calculate.js
@@ -6,6 +6,16 @@ function addDate() {
   document.getElementById("rm1width").value = "";
   document.getElementById("rm1height").value = "";
   document.getElementById("rm1cost").value = "";
+
+  document.getElementById("rm2length").value = "";
+  document.getElementById("rm2width").value = "";
+  document.getElementById("rm2height").value = "";
+  document.getElementById("rm2cost").value = "";
+
+  document.getElementById("rm3length").value = "";
+  document.getElementById("rm3width").value = "";
+  document.getElementById("rm3height").value = "";
+  document.getElementById("rm3cost").value = "";
 }
 
 function showScreen() {
@@ -32,6 +42,26 @@ function estimate() {
   let cost = (totalSqFt * .65);
   document.getElementById("rm1cost").value = cost;
 
-  var totalCost = cost;
+  let length2 = parseFloat(document.getElementById("rm2length").value);
+  let width2 = parseFloat(document.getElementById("rm2width").value);
+  let height2 = parseFloat(document.getElementById("rm2height").value);
+  let totalSqFt2 = ((length2 * width2 * 2) + (length2 * height2 * 2))
+  let cost2 = (totalSqFt2 * .65);
+  document.getElementById("rm2cost").value = cost2;
+
+  let length3 = parseFloat(document.getElementById("rm3length").value);
+  let width3 = parseFloat(document.getElementById("rm3width").value);
+  let height3 = parseFloat(document.getElementById("rm3height").value);
+  let totalSqFt3 = ((length3 * width3 * 2) + (length3 * height3 * 2))
+  let cost3 = (totalSqFt3 * .65);
+  document.getElementById("rm3cost").value = cost3;
+
+  if (numRooms == 1) {
+    var totalCost = cost;
+  } else if (numRooms == 2) {
+    totalCost = cost + cost2
+  } else if (numRooms == 3) {
+    totalCost = cost + cost2 + cost3
+  }
   document.getElementById("estimate").innerHTML = (name + ", your estimate is $" + totalCost + ".");
 }
