1e0e4b22feeca1c5c200c08fac4b8be07fe5a6e7
jonahwhitney <jonahwhitney@yahoo.com>
2025-01-27 10:20:58 -0600

    added estimate function to calculate.js

--- learn04-start/scripts/calculate.js
+++ learn04-start/scripts/calculate.js
@@ -1,4 +1,23 @@
 function addDate() {
   let today = new Date();
-  document.getElementById("myDate").innerHTML = today
+  document.getElementById("myDate").innerHTML = today;
+  document.getElementById("name").value = "";
+  document.getElementById("rm1length").value = "";
+  document.getElementById("rm1width").value = "";
+  document.getElementById("rm1height").value = "";
+  document.getElementById("rm1cost").value = "";
+}
+
+function estimate() {
+
+  let name = document.getElementById("name").value;
+  let length = parseFloat(document.getElementById("rm1length").value);
+  let width = parseFloat(document.getElementById("rm1width").value);
+  let height = parseFloat(document.getElementById("rm1height").value);
+  let totalSqFt = ((length * width * 2) + (length * height * 2))
+  let cost = (totalSqFt * .65);
+  document.getElementById("rm1cost").value = cost;
+
+  var totalCost = cost;
+  document.getElementById("estimate").innerHTML = (name + ", ");
 }
